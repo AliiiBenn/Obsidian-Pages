a0a7bb3c6e5ab6976a49884562b8c492f5edf87d
AliiiBenn <davidddpereiraaa6@gmail.com>
2022-05-22 10:21:53 +0200

    Convertion markdown vers html terminée

--- scripts/main.py
+++ scripts/main.py
@@ -1,56 +1,37 @@
-# import markdown, os
-
-
-# class MardownFile:
-#     def __init__(self, file):
-#         self.file = file
-
-#     def convert_to_string(self) -> str:
-#         with open(self.file, 'r') as f:
-#             text = f.read()
-#         return text
-
-# class ObsidianPage:
-#     def __init__(self, file):
-#         self.file = file
-#         self.html_file = self.convert_to_html()
-
-#     def convert_to_html(self):
-#         return markdown.markdown(self.file)
-
-#     def save_html_file(self):
-#         filepath = os.path.join('web\\pages', "hello_world.html")
-#         if not os.path.exists(filepath):
-#             with open(filepath, 'w') as f:
-#                 f.write(self.html_file)
-
-
-# # print(markdown.markdown("""
-# # # Hello World
-
-# # hello, my name is **Markdown**.
-
-# # - test
-# # - test2"""))
-
-
-# md = MardownFile('test.md')
-# my_first_page = ObsidianPage(md.convert_to_string())
-# my_first_page.save_html_file()
-
-
 from flask import Flask, render_template
+import markdown, os 
+import sys
+import unicodedata
+
+
+def convert_markdown_to_string(mardown_file) -> str:
+    with open(mardown_file, 'r') as mark:
+        text = mark.read()
+    return text
+
+
+def convert_markdown_string_to_html(markdown_string):
+    return markdown.markdown(markdown_string)
+
+def create_new_page_structure(app, page_title, page_content):
+    with app.app_context():
+        rendered = render_template('blog.html', title=page_title, content=page_content)
+    return rendered
+
+def create_html_page(file_name, content):
+    filepath = os.path.join('web/pages', f'{file_name}.html')
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as f:
+            f.write(content)
+
+
+markdown_string = convert_markdown_to_string("test.md")
+html_string = convert_markdown_string_to_html(markdown_string)
+
+# print(html_string)
 
 app = Flask(__name__, template_folder="../web/templates")
 
-@app.route('/')
-def hello_world():
-    return "<h1>Hello World!</h1>"
+page_structure = create_new_page_structure(app, 'test', html_string)
 
-
-if __name__ == '__main__':
-    with app.app_context():
-        rendered = render_template('blog.html', \
-            title = "My Generated Page", \
-            content = "Hello World")
-        print(rendered)
+create_html_page('test', page_structure)
--- test.md
+++ test.md
@@ -1,3 +1,13 @@
-# Hello, World!
+[[Home]]
 
-what about you ?
+---
+
+## MOC Mental
+
+Dans le monde d'aujourd'hui nous devons constamment penser à des dizaines de choses, on ne s'arrete presque jamais, au final on ne pense pas à notre propre personne et à comment nous améliorer. Un problème que beaucoup de personnes ont est qu'ils pense déjà être des experts dans des domaines car ce sont des domaines de la vie quotidienne. Si je dis qu'une personne ne sait pas *dormir* ou même qu'elle ne sait pas [[apprendre]] cela va l'étonner car pour elle ces choses ne s'apprennent pas car elles sont avec nous depuis notre naissance. Pourtant, la connaissance du [[Le sommeil|sommeil]] est assez faible pour beaucoup, idem pour les techniques [[apprendre|d'apprentissage]], les seuls que nous avons connus sont celles que nous avons développé nous même car oui, personnne ne nous apprends à apprendre. C'est pour cela qu'il faut chercher à connaitre plus de choses sur ces sujets.
+
+[[Le travail]], cette action si difficile pour beaucoup car beaucoup moins passionnante qu'une serie ou un film mais malheureusement, dans notre société nous devons tous travailler alors autant bien le faire non ? Comme tous les autres domaines, travailler s'apprend, savoir optimiser son travail pour être plus [[La productivité|productif]] n'est pas si simple. Améliorer ses techniques de travail permettent d'être plus efficace et de gagner du temps, au final on gagne beaucoup de temps à apprendre.
+
+Notre société est bruyante, trop bruyante, nous vivons dans un monde où nous sommes constamment stressés, tendus, avec notre esprit constamment occupé etc... c'est ici que [[La méditation]] est importante. Elle va nous permettre de nous concentrer à nouveau, nous calmer et nous rendre à nouveaux heureux et détendus.
+
+[[L'autisme|Les autistes asperger]], [[les haut potentiels intellectuels]] sont selon moi des personnes extrèmement passionnantes et intéréssantes, ces personnes voient le monde différement, pour beaucoup ils ont peu de [[Les sentiments|sentiments]] mais sont remplis de qualités que peu de personnes "normales" ont. Je pense que nous avons encore beaucoup à apprendre sur ces personnes.
